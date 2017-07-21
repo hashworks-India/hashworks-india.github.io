d06c0bef069afb3540d3c0b85cd1d7745af5df36
chandrasekar <chandrasekar.marappan@hashworks.co>
2017-07-21 12:23:49 +0530

    favicon

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3257,15 +3257,18 @@ $(".showpdf").submit(function (e) {
 
 
 
+
+
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/588707794025667c4ca56d4d/default';
+s1.src='https://embed.tawk.to/5971a0675dfc8255d623e2bb/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+
 
 if (screen.width > 767) {
 function PopUp(hideOrshow) {
